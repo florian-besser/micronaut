d1845a72993c1403f93f45757fbcbc735270b52d
Florian Besser <f.besser@hotmail.com>
2019-11-19 15:17:22 +0700

    First (wrong) PI computation impl

--- src/main/java/com/baeldung/micronaut/helloworld/server/controller/PiComputerController.java
+++ src/main/java/com/baeldung/micronaut/helloworld/server/controller/PiComputerController.java
@@ -9,7 +9,6 @@ import java.math.RoundingMode;
 @Controller("/pi")
 public class PiComputerController {
 
-    private static final BigDecimal MINUS_ONE = new BigDecimal(-1);
     private static final BigDecimal TWO = new BigDecimal(2);
     private static final BigDecimal FOUR = new BigDecimal(4);
 
@@ -17,17 +16,16 @@ public class PiComputerController {
     @Consumes({MediaType.TEXT_PLAIN})
     @Produces({MediaType.TEXT_PLAIN})
     public String compute(@QueryValue int decimals) {
-        // This implementation is NOT good enough in terms of accuracy!
-        long iterations = (long) (5L * Math.pow(10, decimals - 1));
+        long iterations = (long) Math.pow(10, decimals);
         BigDecimal result = new BigDecimal(0);
         for (long i = 0; i < iterations; i++) {
             BigDecimal augend = BigDecimal.ONE.setScale(1000, RoundingMode.UNNECESSARY)
                     .divide(TWO.multiply(new BigDecimal(i)).add(BigDecimal.ONE), RoundingMode.UP);
             if (i % 2 == 1) {
-                augend = augend.multiply(MINUS_ONE);
+                augend = augend.negate();
             }
             result = result.add(augend);
         }
-        return result.multiply(FOUR).setScale(decimals, RoundingMode.UP).toString();
+        return result.multiply(FOUR).setScale(decimals, RoundingMode.HALF_UP).toString();
     }
 }
