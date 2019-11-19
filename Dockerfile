445c68021734dda2aa72de81673015f14bd6c62d
Florian Besser <f.besser@hotmail.com>
2019-11-19 14:25:45 +0700

    Added StringReverser

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:13-alpine
 COPY target/micronaut-0.1.jar /usr/src/greeter/
 WORKDIR /usr/src/greeter
 
-CMD ["java", "-jar", "micronaut-0.1.jar"]
+CMD ["java", "-Xmx16G", "-Xms16G", "-jar", "micronaut-0.1.jar"]
---
+++ src/main/java/com/baeldung/micronaut/helloworld/server/controller/StringReverserController.java
@@ -0,0 +1,14 @@
+package com.baeldung.micronaut.helloworld.server.controller;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+
+@Controller("/reverse")
+public class StringReverserController {
+    @Post("/")
+    @Consumes({MediaType.TEXT_PLAIN})
+    @Produces({MediaType.TEXT_PLAIN})
+    public String greet(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
+}
